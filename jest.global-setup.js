f936cf1c181d3fa8a1206491b8583b2582ed5658
Koichi Murakami <koichi.murakami@mbc-net.com>
2026-04-06 17:18:47 +0900

    chore: remove jest.global-setup.js (no longer needed after c8 migration)

--- jest.global-setup.js
+++
@@ -1,8 +0,0 @@
-// Workaround for Node.js v20.10.0+ bug where NODE_V8_COVERAGE causes
-// an infinite FinalizationRegistry flush loop on Linux/GitHub Actions.
-// https://github.com/nodejs/node/issues/49344
-module.exports = async () => {
-  if (process.env.NODE_V8_COVERAGE) {
-    global.FinalizationRegistry = class {};
-  }
-};
